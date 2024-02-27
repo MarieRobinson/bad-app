da50184e9bcb4491b002389eced6ceb9038a634c
Marie Robinson <mariethe3rd.dev@gmail.com>
2024-02-27 11:01:03 -0600

    move state to the app

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -2,16 +2,18 @@
   <h3>History</h3>
   <ul id="list" class="list">
     <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
-      {{ transaction.text }} <span>{{ transaction.amount }}</span><button class="delete-btn">x</button>
+      {{ transaction.text }} <span>${{ transaction.amount }}</span><button class="delete-btn">x</button>
     </li>
   </ul>
 </template>
 
 <script setup>
-    const transactions = [
-      { id: 1, text: 'Flower', amount: -20 },
-      { id: 2, text: 'Salary', amount: 300 },
-      { id: 3, text: 'Book', amount: -10 },
-      { id: 4, text: 'Camera', amount: 150 }
-    ];
-</script>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true
+  }
+});
+</script> 
